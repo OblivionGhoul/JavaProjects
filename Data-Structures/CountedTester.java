cb130a862d9043f0bc19d0dbd6ef2a7e4bd0afe4
Minh Dinh <mdinh49@gmail.com>
2021-08-22 22:34:50 -0700

    Bug Fixes

--- Data-Structures/CountedTester.java
+++ Data-Structures/CountedTester.java
@@ -1,16 +1,26 @@
+import java.net.MalformedURLException;
 import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class CountedTester {
-
     /**
-    *  @throws Exception
-    *  Creates JOptionPane message with text from args
-    */
-    public static void main(String[] args) throws Exception {
+     *  @throws MalformedURLException
+     *  Creates JOptionPane message with text from args
+     */
+    public static void main(String[] args) throws MalformedURLException {
         URL imageLocation = new URL("https://code.bcp.org/anc/bellarmine-college-preparatory-logo.jpg");
         JOptionPane.showMessageDialog(null, args[0], "Welcome", JOptionPane.PLAIN_MESSAGE, new ImageIcon(imageLocation));
+
+        Singleton.getSingletonInstance();
+
+        Counted count1 = new Counted();
+        Counted count2 = new Counted();
+        Counted count3 = new Counted();
+
+        System.out.println(count1.getCount());
+        System.out.println(count2.getCount());
+        System.out.println(count3.getCount());
     }
 }
 
@@ -33,7 +43,7 @@ class Counted {
     private static int count;
 
     /**
-     * Counts the the number of instances of Counted
+     * Counts the number of instances of Counted
      */
     public Counted() {
         count++;

a386b5e49f13688e03c9d8e3046c53fb31b8a3be
Minh Dinh <mdinh49@gmail.com>
2021-08-30 20:28:31 -0700

    Create ArrayWork.java

---
+++ Data-Structures/ArrayWork.java
@@ -0,0 +1,42 @@
+//R7.27
+/**
+
+//2nd Part
+for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLUMNS; j++) {
+        values[i][j] = (i+j) % 2;
+    }
+}
+
+//4th Part
+for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLUMNS; j++) {
+        sum = sum + values[i][j];
+    }
+}
+*/
+
+
+
+//R7.30
+/**
+a. True
+b. False
+c. False
+d. False
+e. False
+f. True
+g. True
+*/
+
+
+
+//R7.32
+/**
+a. True
+b. True
+c. False
+d. True
+e. False
+f. True
+*/

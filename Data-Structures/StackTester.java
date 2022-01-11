9cace27afad30885a907490597e7d2ade03f5863
Minh Dinh <mdinh49@gmail.com>
2022-01-10 18:28:57 -0800

    Create StackTester.java

---
+++ Data-Structures/StackTester.java
@@ -0,0 +1,77 @@
+import java.util.NoSuchElementException;
+
+public class StackTester {
+    public static void main(String[] args) {
+        StackArray sa = new StackArray();
+        sa.push("a");
+        sa.push("b");
+        sa.push("c");
+        sa.push("d");
+        sa.push("e");
+        System.out.println(sa);
+        System.out.println(sa.pop());
+        System.out.println(sa);
+        System.out.println(sa.pop());
+        System.out.println(sa);
+        System.out.println(sa.pop());
+        System.out.println(sa);
+        System.out.println(sa.pop());
+        System.out.println(sa);
+        System.out.println(sa.pop());
+        System.out.println(sa);
+        System.out.println(sa.pop());
+    }
+}
+
+class StackArray {
+    private Object[] item; // The array where elements are stored
+    private int open = 0;  // The index of the first empty location in the stack
+    private int size = 2;  // The current number of item locations in the stack
+
+    /**
+     Constructs an empty stack.
+     */
+    public StackArray() {
+        item = new Object[size];
+    }
+
+    public void push(Object element) {
+        if (open == item.length) {
+            size *= 2;
+            Object[] arr = new Object[size];
+            for (int i = 0; i < item.length; i++) {
+                arr[i] = item[i];
+            }
+            item = arr;
+        }
+
+        item[open] = element;
+        open++;
+    }
+
+    public Object pop() {
+        if (isEmpty()) throw new NoSuchElementException("Stack Is Empty");
+
+        Object obj = item[open - 1];
+        item[open - 1] = null;
+        open--;
+        return obj;
+    }
+
+    public boolean isEmpty() {
+        return open == 0;
+    }
+
+    public String toString() {
+        if (open == 0) { return "[]"; }
+        String temp = "[" + item[0];
+        int i = 1;
+        while (i < open)
+        {
+            temp = temp + ", " + item[i];
+            i = i + 1;
+        }
+        temp = temp + "]";
+        return temp;
+    }
+}

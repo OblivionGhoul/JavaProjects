9f41ff3f0df07f0c7f6f36f76aecd4831f8de862
OblivionGhoul <45471929+OblivionGhoul@users.noreply.github.com>
2022-04-04 11:38:54 -0700

    Create HeapTester.java

---
+++ Data-Structures/HeapTester.java
@@ -0,0 +1,85 @@
+public class HeapTester {
+    public static void main(String[] args) {
+        Heap<Integer> heap = new Heap<>(4);
+        heap.insert(65);
+        heap.insert(45);
+        heap.insert(35);
+        heap.insert(22);
+        heap.insert(41);
+        heap.insert(10);
+        heap.insert(17);
+        heap.insert(16);
+        heap.insert(14);
+        heap.insert(36);
+        heap.insert(38);
+        heap.insert(4);
+        heap.insert(6);
+        heap.insert(7);
+        heap.print();
+    }
+}
+
+class Heap<E extends Comparable<E>> {
+    private Comparable<E>[] heap;
+    private int size;
+    private int maxSize;
+
+    public Heap(int maxSize) {
+        this.maxSize = maxSize;
+        this.size = 0;
+        this.heap = (E[]) new Comparable[maxSize];
+    }
+
+    public void insert(Comparable<E> e) {
+        if (size == maxSize) {
+            maxSize *= 2;
+            Comparable<E>[] tempArr = new Comparable[maxSize];
+            System.arraycopy(heap, 0, tempArr, 0, heap.length);
+            heap = tempArr;
+        }
+
+        heap[size++] = e;
+        int current = size - 1;
+
+        while (heap[current].compareTo((E) heap[parent(current)]) > 0) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+
+    public void print() {
+        System.out.println(" " + heap[0]);
+
+        for(int i = 0; i < size / 2; i++) {
+            if (leftChild(i) < size) {
+                System.out.print(" " + heap[leftChild(i)]);
+            }
+
+            if (rightChild(i) < size) {
+                System.out.print(" " + heap[rightChild(i)]);
+            }
+
+            if (i == 0 || i == 2 || i == 8) {
+                System.out.println();
+            }
+        }
+    }
+
+    private int parent(int pos) {
+        return (pos - 1) / 2;
+    }
+
+    private int leftChild(int pos) {
+        return (2 * pos) + 1;
+    }
+
+    private int rightChild(int pos) {
+        return (2 * pos) + 2;
+    }
+    
+    private void swap(int pos1, int pos2) {
+        E temp = (E) heap[pos1];
+        heap[pos1] = heap[pos2];
+        heap[pos2] = temp;
+    }
+}
